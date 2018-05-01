335c71e0b4ff453183a7d86a4afc2d0d3678786e
Liguogui <tony@gmail.com>
2018-05-01 17:41:43 +0800

    city router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,22 @@
+<template>
+  <city-header></city-header>
+</template>
+
+<script>
+    import CityHeader from './components/Header.vue'
+    export default{
+        name:'City',
+        data(){
+            return{
+
+            }
+        },
+        components:{
+          CityHeader
+        }
+    }
+</script>
+
+<style rel="stylesheet/stylus" lang="stylus" scoped>
+
+</style>
---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="header">
+      城市选择
+      <router-link to="/">
+        <div class="iconfont header-back">&#xe624;</div>
+      </router-link>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'CityHeader',
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style rel="stylesheet/stylus" lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .header
+    position:relative
+    overflow: hidden
+    height: $headerHeight
+    line-height: .86rem
+    background-color: $bgColor
+    color: #ffffff
+    text-align: center
+    font-size: .32rem
+    .header-back
+      position:absolute
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      top: 0
+      left: 0
+      color: #fff
+</style>
