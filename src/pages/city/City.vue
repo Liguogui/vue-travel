86b15042f22a1f58c654f2ff6e350eed4ce889b4
Liguogui <tony@gmail.com>
2018-05-01 21:06:47 +0800

    city search

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,9 +1,13 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
     import CityHeader from './components/Header.vue'
+    import CitySearch from './components/Search.vue'
     export default{
         name:'City',
         data(){
@@ -12,7 +16,8 @@
             }
         },
         components:{
-          CityHeader
+          CityHeader,
+          CitySearch
         }
     }
 </script>
