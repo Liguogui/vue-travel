d793716fc1055d655a61f658a2a2bd27673813a1
Liguogui <tony@gmail.com>
2018-05-02 15:26:46 +0800

    city component2

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -31,8 +31,14 @@
         },
         data(){
             return{
-              touchStatus:false  //标识位
+              touchStatus:false,  //标识位
+              startY:0,
+              timer:null
             }
+        },
+        updated(){     //重新渲染时调用
+            this.startY = this.$refs['A'][0].offsetTop;
+            console.log(this.startY);
         },
         methods:{
           handLetterClick(e){
@@ -42,17 +48,30 @@
             this.touchStatus = true;
           },
           handleTouchMove(e){
+            let me = this;
             if(this.touchStatus){
+              if(this.timer){
+                clearTimeout(this.timer);
+              }
+              this.timer = setTimeout(function () {
+                const touchY = e.touches[0].clientY;
+                const distandtouchY = touchY - 79;
+                const index = Math.floor((distandtouchY - me.startY)/20);
+                if(index >= 0 && index < me.letters.length){
+                  me.$emit('change',me.letters[index]);
+                }
+              },16);
+
 //              e.touches[0]表示手指的一些信息
-              const startY = this.$refs['A'][0].offsetTop;
-              const touchY = e.touches[0].clientY;
-              const distandtouchY = touchY - 79;
-              const index = Math.floor((distandtouchY - startY)/20);
+//              const startY = this.$refs['A'][0].offsetTop;
+//              const touchY = e.touches[0].clientY;
+//              const distandtouchY = touchY - 79;
+//              const index = Math.floor((distandtouchY - this.startY)/20);
 //              console.log(index);
 //              console.log(this.letters[index]);
-              if(index >= 0 && index < this.letters.length){
-                this.$emit('change',this.letters[index]);
-              }
+//              if(index >= 0 && index < this.letters.length){
+//                this.$emit('change',this.letters[index]);
+//              }
 //              console.log(e.touches[0]);
 //              console.log(e.touches);
             }
